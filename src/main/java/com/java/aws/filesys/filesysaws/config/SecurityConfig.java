45f7f9a6dba1bcdd62b00a4d535d42d576792473
Bogdan Atakshiev <atakshievbogdan@gmail.com>
2021-08-22 23:47:23 +0300

    v.0.0.5 - Add Swagger

--- src/main/java/com/java/aws/filesys/filesysaws/config/SecurityConfig.java
+++ src/main/java/com/java/aws/filesys/filesysaws/config/SecurityConfig.java
@@ -5,6 +5,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
@@ -31,6 +32,16 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
                 .antMatchers("/files/**").hasAnyRole("ADMIN", "MODERATOR")
                 .antMatchers("/events/**").hasAnyRole("ADMIN", "USER")
                 .anyRequest().authenticated();
+    }
+
+    @Override
+    public void configure(WebSecurity web) throws Exception {
+        web.ignoring().antMatchers("/v2/api-docs",
+                "/configuration/ui",
+                "/swagger-resources/**",
+                "/configuration/security",
+                "/swagger-ui.html",
+                "/webjars/**");
     }
 
 
